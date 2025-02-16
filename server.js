765d658ad57c64846eba08be7393ccaf5a06f913
Jivan052 <jamadarjivan01@gmail.com>
2025-02-16 19:38:15 +0530

    Chnages

--- server.js
+++ server.js
@@ -2,12 +2,19 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+// Serving html file
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 const rooms = {};
+
+// Serve index.html at the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 wss.on('connection', (ws) => {
   console.log('New WebSocket connection');
